f647554e20914177af83f9b88fbf3eee941b54c9
Danilo Ferreira <155866296+danferreira011@users.noreply.github.com>
2026-06-06 20:07:23 -0300

    backend correção

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "pipeline-terraform-ansible-state"
+    bucket         = "pipeline-terraform-ansible-state-dsf"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
+    dynamodb_table = "terraform-state-lock-dsf"
     encrypt        = true
   }
 }
